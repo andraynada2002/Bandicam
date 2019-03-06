950225566a2aa151bb89e61e467131e2da32f5fd
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-06 11:08:26 +1000

    Add files via upload

---
+++ bandicam.cpp
@@ -0,0 +1,56 @@
+1: void foo(){ 
+2: int x; 
+3: for(int i = 0; i < N; i++) 
+4: x += i; 
+5: }
+void foo(){ 
+int x; 
+for(int i = 0; i < N; i++) 
+x += bar(i); 
+}
+int bar(int i){ 
+return i; 
+}
+for(int i = 0; i < N; i++) 
+*a++ = *b++ + *c++; 
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i]; 
+a += N; 
+b += N; 
+c += N;
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i];
+public class Bandicam;
+public class Neurons;
+operator Neurons;
+friend Neurons;
+register Neurons;
+{
+	//Bandicam video optimized
+	//Bandicam application optimized
+	//Bandicam video recording optimized
+	//Bandicam video recording realtime performance
+	//Bandicam video recording realtime framerate
+	//Bandicam video recording realtime fps
+	//Bandicam video recording quality Good + Best
+	//Bandicam video recording fixed and remove artifacts
+	//Bandicam video recording fixed and remove noise
+	//Bandicam video recording fixed and remove chromatic aberration
+	//Bandicam video recording video Ultra Hight Definition
+	propertyName Bandicam;
+	propertyName Application;
+	propertyName video;
+	propertyName recording;
+	propertyName realtime;
+	propertyName Noise;
+	propertyName artifacts
+	propertyName ChromaticAberration; propertyName Fixed; propertyName performance; propertyName Framerate; propertyName Fps; propertyName UltraHightDefinition;
+}
+abort Neurons;
+abs Neurons;
+absread Neurons;
+abswrite Neurons;
+asm Neurons;
+if Neurons;
+else Neurons;
+
